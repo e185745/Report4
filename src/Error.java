18719dd042c1c1036b570d9125e38d978c659f1a
Sho Uema <e185745@ie.u-ryukyu.ac.jp>
2018-11-26 14:19:20 +0900

    Third commit

--- src/Error.java
+++ src/Error.java
@@ -1,11 +1,6 @@
 public class Error {
-    public static void main(String[] args){
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }catch (NullPointerException e) {
-            System.out.println("NullPointerExceptionが発生しました");
-            System.out.println(e.getMessage());
-        }
+    public static void main(String[] args) {
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
